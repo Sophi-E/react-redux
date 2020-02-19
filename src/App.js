87cc9aa28b50471a725047898f2d42dea1a01cc2
Sophia <sophierithous@gmail.com>
2020-02-19 16:48:42 +0100

    functional component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
 import Ninjas from "./components/Ninjas";
+import AddNinja from "./components/AddNinja";
 
 class App extends Component {
   state = {
     ninjas: [
       { name: "yoshi", age: "25", belt: "black", id: "1" },
-      { name: "yoshi", age: "25", belt: "black", id: "1" },
+      { name: "yoshi", age: "25", belt: "black", id: "2" },
 
-      { name: "yoshi", age: "25", belt: "black", id: "1" }
+      { name: "yoshi", age: "25", belt: "black", id: "3" }
     ]
   };
   render() {
     return (
-      <div>
+      <div className="App">
+        <AddNinja />
         <Ninjas ninjas={this.state.ninjas} />
       </div>
     );
--- src/components/Ninjas.js
+++ src/components/Ninjas.js
@@ -1,20 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Ninjas extends Component {
-  render() {
-    const { ninjas } = this.props;
-    const ninjaList = ninjas.map(ninja => {
-      return (
-        <div key={ninja.id}>
-          <h4>Name: {ninja.name}</h4>
-          <h4>Age: {ninja.age}</h4>
-          <h4>Belt: {ninja.belt}</h4>
-        </div>
-      );
-    });
+const Ninjas = ({ ninjas }) => {
+  const ninjaList = ninjas.map(ninja => {
+    return (
+      <div key={ninja.id} className="ninja">
+        <h4>Name: {ninja.name}</h4>
+        <h4>Age: {ninja.age}</h4>
+        <h4>Belt: {ninja.belt}</h4>
+      </div>
+    );
+  });
 
-    return <div>{ninjaList}</div>;
-  }
-}
+  return <div>{ninjaList}</div>;
+};
 
 export default Ninjas;
